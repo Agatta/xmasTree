62660661962abf71ac0d0db6aff32906dbe44a4a
Agata Skorska (PGS Software) <askorska@pgs-soft.com>
2016-04-13 16:48:11 +0200

    choinka w jedna strone

--- src/main/java/com/pgs/javadev/App.java
+++ src/main/java/com/pgs/javadev/App.java
@@ -1,4 +1,5 @@
 package com.pgs.javadev;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -7,7 +8,54 @@ package com.pgs.javadev;
 public class App {
     public static void main( String[] args) {
         XmasTreeCreator creator =  new XmasTreeCreator();
-        System.out.println(creator.createXmasTree("x", 100));
+        Scanner userScanner = new Scanner(System.in);
+        String text = "";
+        int levels = 0;
+       // System.out.print("Jakiego znaku chcesz uzyc? \n");
+        int length = 0;
+        while (length != 1) {
+            System.out.print("Jakiego znaku chcesz uzyc? \n");
+            text = userScanner.nextLine();
+            length = text.length();
+            if  (length > 1 ) {
+                System.out.println("Za długie");
+                length = text.length();
+            }
+            else {
+                System.out.println("OK");
+                length = 1;
+            }
+
+        }
+
+        while (levels  <= 0)  {
+         System.out.print("Ile poziomow ma miec Twoja wyopasiona choinka? \n");
+            String levelString = userScanner.nextLine();
+            try{
+                levels = Integer.valueOf(levelString);
+                if(levels < 1) {
+                    System.out.println("Minimalina ilość poziomów to 1");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("To nie jest odpowiednia liczba");
+
+            }
+/*
+            if ((userScanner.hasNextInt()) ) {
+                levels = userScanner.nextInt();
+                if (levels  < 1) {
+                    System.out.println("Minimalina ilość poziomów to 1");
+                }
+            }
+            else {
+                System.out.println("To nie jest odpowiednia liczba");
+                userScanner.next();
+            }*/
+        }
+
+
+
+        System.out.println(creator.createXmasTree(text, levels));
     }
 
 }
--- src/main/java/com/pgs/javadev/XmasTreeCreator.java
+++ src/main/java/com/pgs/javadev/XmasTreeCreator.java
@@ -1,10 +1,42 @@
 package com.pgs.javadev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class XmasTreeCreator {
 
     public String createXmasTree(String text, int levels) {
-        return "";
+
+        String tree = "";
+
+        for (int i = 0; i < levels; i++) {
+            if (i < levels -1 ) {
+                tree += printLevel(levels, i, text) + "\n";
+            }
+            else {
+                tree += printLevel(levels, i, text);
+            }
+
+        }
+
+
+        return tree;
     }
 
+    private String printLevel(int level, int currentLevel, String text) {
+        String space = "";
+        String body = "";
+        for (int i = 0; i < level - currentLevel - 1; i++) {
+            space += " ";
+        }
+        for (int i = 0; i < currentLevel * 2 + 1; i++) {
+            body += text;
+        }
+        space +=  body;
+        return space;
+
+    }
 
 }
+
+
